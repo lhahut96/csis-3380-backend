2bfc7ce30ee35d98f1290bb0da2ef8ef581e0f83
Lucas <lhahut96@gmail.com>
2024-07-28 19:45:24 -0700

    update db

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,3 +1,3 @@
 module.exports = {
-  uri: "mongodb+srv://fullstackdb:6MX5CAcVIV6TAYQp@lucasmongo.9rxonsd.mongodb.net/?retryWrites=true&w=majority&appName=lucasMongo",
+  uri: "mongodb+srv://lucas:lucas@summer24.fo7hfq4.mongodb.net/?retryWrites=true&w=majority&appName=fullstackproject",
 };
