9b30ea6382bc247a48063675e80ee3b885105eec
Lucas <lhahut96@gmail.com>
2024-07-28 20:28:44 -0700

    using cors

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ var router = require("./routes/index");
 
 var app = express();
 
+app.use(cors());
+
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
